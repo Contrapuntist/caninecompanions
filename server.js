bc2a20d887e750383d8d3acdda5c08da99bf812d
Contrapuntist <contrapuntist@gmail.com>
2017-08-21 20:19:03 -0500

    basic server working. Sequelize server start up code commented out.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
 // Sets up the Express App
 // =============================================================
@@ -13,19 +14,34 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./app/models");
+// var db = require("./models");
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Static directory
 app.use(express.static("public")); 
 
-db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
-      console.log("App listening on PORT " + PORT);
-    });
-  });
+
+// *******************************************************
+//      Sequelize code to start server
+//      -- Uncommenting 'db' variable above required
+// *******************************************************
+
+// db.sequelize.sync().then(function() {
+//     app.listen(PORT, function() {
+//       console.log("App listening on PORT " + PORT);
+//     }); 
+//   });
+
+// ********************************************
+//      For testing server without sequelize 
+//      -- Comment out when sequelize required 
+// ********************************************
+
+app.listen(PORT, function() { 
+    console.log('app listening on port ' + PORT); 
+});
+
+
