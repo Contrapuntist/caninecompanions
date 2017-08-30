b04ae45faa05538dd1628ae757e9467961713010
Contrapuntist <contrapuntist@gmail.com>
2017-08-29 21:44:52 -0500

    results rendering with stub data. Also add hide/show on results div

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,8 @@
+
 $(document).ready(function () {
-    
+    $("#results").hide();
+
+
     // jQuery EVENT LISTENERS 
     $("#submitbtn").on("click", function (e) { 
         e.preventDefault(); 
@@ -57,7 +60,24 @@ $(document).ready(function () {
         // VALUE FOR TESTING BREED MATCH GET REQUEST
         // var testbreedmatch = "smallhomelighthighenergy";
 
-        findBreed(formInput); 
+        var userInput = {		
+            lastName: "Doe", // frmLastName,		
+            firstName: "Jane", // frmFirstName, 		
+            email: "jdoe1234@gmail.com", // frmEmail,		
+            password: '1234567', // frmPassword,		
+            zip: "60601", // frmZip 		
+            dogSex: "male", // frmDogGender		
+            dogAge: "baby", // frmDogAge		
+            dogSize: "small", // frmDogSize 		
+            dogHome: "home", // frmDogHome		
+            dogHair: "light", // frmDogShed		
+            dogEnergy: "calm", // frmDogEnergy		
+            testbreedmatch: function () {		
+               return this.dogSize + this.dogHome + this.dogHair + this.dogEnergy		
+            }		
+        }
+
+        findBreed(userInput); 
      
 
         // CREATE NEW USER 
@@ -181,21 +201,21 @@ $(document).ready(function () {
                 data: querystring
             }).done(function(response) { 
 
-        for (i = 1; i < 10; i++) {
-        document.getElementById("images" +i).src = response.petfinder.pets.pet[i].media.photos.photo[3].$t;
+               console.log(response.petfinder) 
+                for (i = 1; i < 10; i++) {
+                    document.getElementById("images" +i).src = response.petfinder.pets.pet[i].media.photos.photo[3].$t;
 
-        $(".name" +i).html(response.petfinder.pets.pet[i].name.$t );
-        $(".age" +i).html("Age: " + response.petfinder.pets.pet[i].age.$t);
-        $(".city" +i).html("City: " + response.petfinder.pets.pet[i].contact.city.$t);
-        $(".contact" +i).html("Contact: " + response.petfinder.pets.pet[i].contact.email.$t );
-        $(".sex" +i).html("Sex: " + response.petfinder.pets.pet[i].sex.$t);
-        $(".size" +i).html("Size: " + response.petfinder.pets.pet[i].size.$t );
+                    $(".name" +i).html(response.petfinder.pets.pet[i].name.$t );
+                    $(".age" +i).html("Age: " + response.petfinder.pets.pet[i].age.$t);
+                    $(".city" +i).html("City: " + response.petfinder.pets.pet[i].contact.city.$t);
+                    $(".contact" +i).html("Contact: " + response.petfinder.pets.pet[i].contact.email.$t );
+                    $(".sex" +i).html("Sex: " + response.petfinder.pets.pet[i].sex.$t);
+                    $(".size" +i).html("Size: " + response.petfinder.pets.pet[i].size.$t );
 
-        var details = document.querySelector('.description' +i);
-        details.setAttribute('data-balloon', response.petfinder.pets.pet[i].description.$t);
-
-            };
-        });
+                    var details = document.querySelector('.description' +i);
+                    details.setAttribute('data-balloon', response.petfinder.pets.pet[i].description.$t);
+                };
+            });
         }
         
         // WOLFRAM API 'PROXY' CALL THROUGH SERVER/ROUTING
@@ -206,13 +226,14 @@ $(document).ready(function () {
                 method: 'get',
                 url: wolfqrstr
             }).done(function(response) { 
-    
-                console.log('back in wolfram api call'); 
+                
+                $('#results').show();
+                // console.log('back in wolfram api call'); 
+                //   console.log(response); 
                 // $('#app').html(res);
-                 $(".dogInfo").html("<b>About: </b>" + response.queryresult.pods[4].subpods[0].plaintext + "<br>");
-                  console.log(response); 
-                  $(".dogName").html(response.queryresult.pods[0].subpods[0].plaintext  );
-                  $(".dogHistory").html("<b> History: </b>" + response.queryresult.pods[6].subpods[0].plaintext );
+                $(".dogInfo").html("<b>About: </b>" + response.queryresult.pods[4].subpods[0].plaintext + "<br>");
+                $(".dogName").html(response.queryresult.pods[0].subpods[0].plaintext  );
+                $(".dogHistory").html("<b> History: </b>" + response.queryresult.pods[6].subpods[0].plaintext );
         
             });
         }
